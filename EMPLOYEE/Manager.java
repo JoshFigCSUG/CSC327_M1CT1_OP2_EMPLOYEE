675f5ac2a34abb582dd62d54152e484f0d0aff01
JoshFigCSUG <joshua.figueroa@csuglobal.edu>
2024-08-21 17:55:11 -0600

    Update Manager.java
    create subclass specific fields (department) and constructor for Manager class

--- EMPLOYEE/Manager.java
+++ EMPLOYEE/Manager.java
@@ -1,3 +1,16 @@
 public class Manager extends Employee {
+    
+    private String department;
+
+    // Constructor
+    public Manager(String firstName, String lastName, int employeeID, String department) {
+        super(firstName, lastName, employeeID); // Pass parameters to the superclass constructor
+        this.department = department;
+    }
+
+    // Setter for the department field
+    public void setDepartment(String department) {
+        this.department = department;
+    }
 
 }
